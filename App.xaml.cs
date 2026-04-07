ba48a8e10ce24da6a6288c374305e394e2206de9
Roeddy Bodja Mbengi <188416990+Roeddy-Bodja-Mbengi@users.noreply.github.com>
2026-04-07 10:59:27 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Digitale_Meter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
